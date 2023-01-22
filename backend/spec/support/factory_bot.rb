7e52fce5a39e712d83ef92536dd678b87a25bd79
Marcio de Jesus <marciodejesusrj@gmail.com>
2023-01-22 13:49:58 -0300

    rubocoping

--- backend/spec/support/factory_bot.rb
+++ backend/spec/support/factory_bot.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
-  config.before(:all)  { FFaker::Random.seed = config.seed }
-  config.before(:each) { FFaker::Random.reset! }
+  config.before(:all) { FFaker::Random.seed = config.seed }
+  config.before { FFaker::Random.reset! }
 end
