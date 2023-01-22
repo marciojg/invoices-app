6ded3e01f0183f28fa34597df2c931b62e8f108b
Marcio de Jesus <marciodejesusrj@gmail.com>
2023-01-22 14:43:03 -0300

    Change sequential id to use uuid

--- backend/db/migrate/20230122173834_create_invoices.rb
+++ backend/db/migrate/20230122173834_create_invoices.rb
@@ -2,7 +2,7 @@
 
 class CreateInvoices < ActiveRecord::Migration[7.0]
   def change
-    create_table :invoices do |t|
+    create_table :invoices, id: :uuid do |t|
       t.integer :number, null: false
       t.date :reference_date, null: false
       t.text :company_data, null: false
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,19 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_122_160_315) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_22_173834) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "pgcrypto"
+  enable_extension "plpgsql"
 
-  create_table 'invoices', force: :cascade do |t|
-    t.integer 'number', null: false
-    t.date 'reference_date', null: false
-    t.text 'company_data', null: false
-    t.text 'billing_data', null: false
-    t.integer 'value_cents', default: 0, null: false
-    t.string 'value_currency', default: 'BRL', null: false
-    t.string 'emails', default: [], array: true
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "invoices", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.integer "number", null: false
+    t.date "reference_date", null: false
+    t.text "company_data", null: false
+    t.text "billing_data", null: false
+    t.integer "value_cents", default: 0, null: false
+    t.string "value_currency", default: "BRL", null: false
+    t.string "emails", default: [], array: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
