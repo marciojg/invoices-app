59a414dd623b8962553d68d237f938779ba39744
Marcio de Jesus <marciodejesusrj@gmail.com>
2023-01-22 21:57:50 -0300

    Creates `SendEmail::NormalizeParams` case

---
+++ backend/app/ucase/invoices/send_email/normalize_params.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Invoices::SendEmail
+  class NormalizeParams < Base
+    def call!
+      normalized_emails = emails.map { |email| String(email).downcase.strip } if emails.present?
+
+      Success result: attributes.merge(emails: normalized_emails)
+    end
+  end
+end
