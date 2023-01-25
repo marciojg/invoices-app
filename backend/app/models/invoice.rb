7150e51566b1aeb486a7d3fed739dfdbb764b155
Marcio de Jesus <marciodejesusrj@gmail.com>
2023-01-25 13:04:34 -0300

    Custom validation of `reference_date`

--- backend/app/models/invoice.rb
+++ backend/app/models/invoice.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class Invoice < ApplicationRecord
-  validates :reference_date, :company_data, :billing_data, :emails, presence: true
+  validates :company_data, :billing_data, :emails, presence: true
   validates :number, :value_cents, numericality: { greater_than: 0 }
   validates :number, uniqueness: true
 
   validate :validate_emails
+  validate :validate_reference_date
 
   def self.ransackable_attributes(auth_object = nil)
     super & %w[number reference_date]
@@ -18,5 +19,11 @@ class Invoice < ApplicationRecord
     return unless emails.any? { |email| (email =~ URI::MailTo::EMAIL_REGEXP).nil? }
 
     errors.add :emails, 'some of the ones are invalid'
+  end
+
+  def validate_reference_date
+    Date.parse(reference_date.to_s)
+  rescue
+    errors.add :reference_date, 'is not a valid date'
   end
 end
--- backend/spec/models/invoice_spec.rb
+++ backend/spec/models/invoice_spec.rb
@@ -4,7 +4,6 @@ require 'rails_helper'
 
 RSpec.describe Invoice do
   describe 'validations' do
-    it { is_expected.to validate_presence_of(:reference_date) }
     it { is_expected.to validate_presence_of(:company_data) }
     it { is_expected.to validate_presence_of(:billing_data) }
     it { is_expected.to validate_presence_of(:emails) }
@@ -24,6 +23,28 @@ RSpec.describe Invoice do
         invoice.valid?
 
         expect(invoice.errors.key?(:emails)).to be true
+      end
+    end
+
+    context 'with invalid reference_date' do
+      context 'with invalid date' do
+        subject(:invoice) { build(:invoice, reference_date: '2020-20-20') }
+
+        it do
+          invoice.valid?
+
+          expect(invoice.errors.key?(:reference_date)).to be true
+        end
+      end
+
+      context 'without value' do
+        subject(:invoice) { build(:invoice, reference_date: nil) }
+
+        it do
+          invoice.valid?
+
+          expect(invoice.errors.key?(:reference_date)).to be true
+        end
       end
     end
   end
