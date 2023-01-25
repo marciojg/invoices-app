51fc7cccccb9f85144e91037fe6d8697728ab654
Marcio de Jesus <marciodejesusrj@gmail.com>
2023-01-25 11:14:47 -0300

    Creates `SendEmail::Delivery` case

---
+++ backend/app/ucase/invoices/send_email/delivery.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Invoices::SendEmail
+  class Delivery < Base
+    def call!
+      invoice.emails.each do |email|
+        InvoiceMailer.with(email:).invoice_email.deliver_later
+      end
+
+      Success result: { **attributes }
+    rescue => e
+      Failure result: { message: e.message }
+    end
+  end
+end
---
+++ backend/spec/ucase/invoices/send_email/delivery_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Invoices::SendEmail::Delivery do
+  include ActiveJob::TestHelper
+
+  describe '.call' do
+    subject(:call) { described_class.call(**params) }
+
+    let(:invoice) { create(:invoice, emails: ['foo@bar.com']) }
+    let(:params) { { id: invoice.id, emails: [], invoice: } }
+
+    context 'when delivery with success' do
+      it 'when invoice_email is called' do
+        expect { call }.to have_enqueued_job(ActionMailer::MailDeliveryJob)
+      end
+
+      it { expect(call.success?).to be true }
+    end
+
+    context 'when raise exception' do
+      before { allow(InvoiceMailer).to receive(:with).with(email: 'foo@bar.com').and_raise(StandardError) }
+
+      it { expect(call.failure?).to be true }
+      it { expect(call.data.key?(:message)).to be true }
+    end
+  end
+end
