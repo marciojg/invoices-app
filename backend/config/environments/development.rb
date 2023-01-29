84d60c4495867213d5e70f4575f6563ffbf7a366
Marcio de Jesus <marciodejesusrj@gmail.com>
2023-01-29 10:07:10 -0300

    Add mailcatcher

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -55,6 +55,11 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3030 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: 'mailcatcher', port: 1025 }
+  # config.action_mailer.raise_delivery_errors = false
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
