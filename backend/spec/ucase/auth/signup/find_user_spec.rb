79aa56e5f99a17b4446fa12d7959328758df5bfa
Marcio de Jesus <marciodejesusrj@gmail.com>
2023-01-30 00:13:52 -0300

    Creates `Signup::FindUser` case

---
+++ backend/spec/ucase/auth/signup/find_user_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Auth::Signup::FindUser do
+  describe '.call' do
+    subject(:call) { described_class.call(**params) }
+
+    let(:params) { { email: } }
+
+    before { create(:user) }
+
+    context 'with a valid email' do
+      let(:email) { User.last.email }
+
+      it { expect(call.success?).to be true }
+      it { expect(call.data[:user]).to be_instance_of(User) }
+    end
+
+    context 'with a invalid email' do
+      context 'when the email is blank' do
+        let(:email) { nil }
+
+        it { expect(call.failure?).to be true }
+        it { expect(call.type).to eq(:invalid_data) }
+        it { expect(call.data[:email]).to be_nil }
+      end
+
+      context 'when the email is not a String' do
+        let(:email) { 123 }
+
+        it { expect(call.failure?).to be true }
+        it { expect(call.type).to eq(:invalid_data) }
+        it { expect(call.data[:email]).to eq(123) }
+      end
+    end
+
+    context 'when not found' do
+      let(:email) { 'foo@bar.com' }
+
+      it { expect(call.success?).to be true }
+      it { expect(call.data[:user]).to be_nil }
+    end
+  end
+end
