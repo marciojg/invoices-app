aa64174986f2a2df5541ab02b3345f1ae0c5496e
Marcio de Jesus <marciodejesusrj@gmail.com>
2023-01-31 21:45:52 -0300

    Creates `Signup::RenewToken` case

---
+++ backend/spec/ucase/auth/signup/renew_token_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Auth::Signup::RenewToken do
+  describe '.call' do
+    subject(:call) { described_class.call(**params) }
+
+    context 'without the user in the params' do
+      let(:params) { { email: 'foo@bar.com' } }
+
+      it { expect(call.success?).to be true }
+      it { expect(call.data[:email]).to eq(params[:email]) }
+      it { expect(call.data[:user]).to be_nil }
+
+      it 'not update the tokens of user' do
+        allow(User).to receive(:update!)
+
+        expect(User).not_to have_received(:update!)
+      end
+
+      it 'not enqueue a renew registration email' do
+        expect { call }.not_to have_enqueued_job(ActionMailer::MailDeliveryJob)
+      end
+    end
+
+    context 'with the user in the params' do
+      let(:params) { { email: 'foo@bar.com', user: create(:user) } }
+
+      it { expect(call.success?).to be true }
+      it { expect(call.data[:email]).to eq(params[:email]) }
+      it { expect(call.data[:user]).to be_instance_of(User) }
+
+      it { expect(call.data[:user].email_confirmed).to be_falsey }
+
+      it 'enqueue a renew registration email' do
+        expect { call }.to have_enqueued_job(ActionMailer::MailDeliveryJob)
+      end
+    end
+  end
+end
