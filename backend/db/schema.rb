c7d7e6b84ddd0ccc3447a717b1328ddd14cb2a09
Marcio de Jesus <marciodejesusrj@gmail.com>
2023-02-01 23:19:39 -0300

    change name of column `confirm_token`  to `confirm_token_digest`

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_26_001006) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_02_021635) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -32,7 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_26_001006) do
     t.string "token_digest"
     t.string "renew_token_digest"
     t.boolean "email_confirmed", default: false
-    t.string "confirm_token"
+    t.string "confirm_token_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
