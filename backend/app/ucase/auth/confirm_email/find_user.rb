d971d7b2877c8506bb1d267e63f7b16413fcff72
Marcio de Jesus <marciodejesusrj@gmail.com>
2023-02-02 21:56:29 -0300

    Creates `ConfirmEmail::FindUser` case

---
+++ backend/app/ucase/auth/confirm_email/find_user.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Auth::ConfirmEmail
+  class FindUser < Base
+    def call!
+      if email.present? && email.is_a?(String)
+        user = User.find_by(email:)
+
+        Success result: { **attributes.merge(user:) }
+      else
+        Failure :invalid_data, result: { email: }
+      end
+    end
+  end
+end
