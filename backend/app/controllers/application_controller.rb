a5447844f593ac2a65ef975b4ca1812b967433af
Marcio de Jesus <marciodejesusrj@gmail.com>
2023-02-03 00:08:09 -0300

    Creates `authenticated?` feature

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,4 +1,29 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  private
+
+  def authenticated?
+    authenticate!
+
+    render json: { error: 'unauthorized' }, status: :unauthorized if Current.user.blank?
+  end
+
+  def authenticate!
+    return if Current.user.present?
+
+    user = User.find_by(email: current_user_email, email_confirmed: true)
+
+    return if user.blank?
+
+    Current.user = user if user.authenticate_token(current_user_token)
+  end
+
+  def current_user_email
+    request.headers['x-client']
+  end
+
+  def current_user_token
+    request.headers['x-access-token']
+  end
 end
